01b008b4a3db4d87583c4268b7a2adeb9bfcc59d
rp <rp@burgeins.de>
2023-03-22 12:01:59 +0100

    * updated the README.md * added composer files to the repo

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ren-papenfu/rene-papenfuss-de',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2eae323c697ced9e922a7bf1f39ecc0cee75c8bc',
+        'type' => 'grav-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ren-papenfu/rene-papenfuss-de' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2eae323c697ced9e922a7bf1f39ecc0cee75c8bc',
+            'type' => 'grav-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
